6c3510ac56b0426a0e7d9b94d4dde24f878df9b5
piyush <pg.bitcoding@gmail.com>
2023-06-21 18:07:41 +0530

    navbar update

--- userauthenticate/admin.py
+++ userauthenticate/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import User
 # Register your models here.
 
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email")
+    search_fields = ("username",)
+
+admin.site.register(User, UserAdmin)
